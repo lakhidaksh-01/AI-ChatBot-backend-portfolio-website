a8551cb35407d26061aa2d06e54a08216089b729
DAKSH LAKHI <dakshlakhi01@gmail.com>
2026-03-24 15:48:20 +0530

    server.js edited

--- server.js
+++ server.js
@@ -72,6 +72,8 @@ JARVIS:
   }
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
